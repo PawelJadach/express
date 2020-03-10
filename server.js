ed22f2a1405243999299219aee387f2aa0295c72
Paweł Jadach <pawel.jadach.96@gmail.com>
2020-03-10 23:40:27 +0100

    add handlebars and new views

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
 
 app.use('/user', (req, res, next) => {
   res.send('<h1>Musisz się zalogować!</h1>')
@@ -17,15 +14,31 @@ app.use('/user', (req, res, next) => {
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index')
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index')
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about')
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark'})
+});
+
+app.get('/info', (req, res) => {
+  res.render('info')
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
